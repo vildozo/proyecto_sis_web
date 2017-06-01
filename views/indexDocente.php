e6cd4f6ed97a357c6b550464ea2dc199bf7eab82
DharaRioja <cuatita_16@hotmail.com>
2017-05-31 20:42:47 -0400

    probando estilitos para vistas

---
+++ views/indexDocente.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+
+<html>
+	<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+	<script src="semantic/dist/semantic.min.js"></script>
+	<title>Index</title>
+	<body>
+	
+	<div class="ui top attached tabular menu">
+		<a class="item">
+	   Noticias
+	  </a>
+	  <a class="item">
+	   Información General
+	  </a>
+		<a class="active item">
+	   Plantel Docente
+	  </a>
+		<a class="item">
+	   Cursos de Formación Continua
+	  </a>
+	</div>
+	<div class="ui bottom attached segment">
+	  <p></p>
+	</div>
+
+  </body>
+</html>
