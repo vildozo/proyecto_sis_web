3287ee25b21fa0a957355ad77a31299d68bdb7f4
DharaRioja <cuatita_16@hotmail.com>
2017-06-10 15:54:52 -0400

    vistas para create de materia v1

---
+++ views/indexMaterias.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML>
+
+<html>
+	<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+	<script src="semantic/dist/semantic.min.js"></script>
+
+	<head>
+		<title>Plantel Docente</title>
+	</head>
+
+	<body>
+
+	<div class="ui container">
+	</br>
+		<div class="ui sticky container segment fluid" style="padding-left:0; padding-right:0; padding-bottom:0;">
+			<div class="ui two column grid">
+				<div class="row">
+
+				<div class="one wide column">
+				</div>
+
+				 <div class="three wide column">
+					 <div class="ui small image">
+	  					<img src="imgs/logoucb.gif">
+						</div>
+					</div>
+
+					<div class="eleven wide column">
+						<div class="ui massive image">
+							<img src="imgs/Ingenieria-de-Sistemas.jpg">
+						</div>
+					</div>
+
+					<div class="one wide column">
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+		<div class="ui inverted menu" style="margin:auto; background-color: #0B0B3B;">
+			<a class="item">Noticias</a>
+			<a class="item">Información General</a>
+			<a class="active item">Plantel Docente</a>
+			<a class="item">Cursos de Formación Continua</a>
+		</div>
+
+		<!--Lista de Materias-->
+		<div class="ui segment">
+      <div class="ui relaxed divided list">
+        <div class="item">
+          <i class="large github middle aligned icon"></i>
+          <div class="content">
+            <a class="header">Semantic-Org/Semantic-UI</a>
+            <div class="description">Updated 10 mins ago</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--Fin Lista-->
+  </div>
+  </br>
+  </br>
+
+    <!--Footer-->
+    <div class="ui center aligned inverted darkgreen vertical footer segment" style="background-color: #0B0B3B">
+       <div class="ui horizontal inverted small divided link list">
+
+        </div>
+    </div>
+    <!--Fin Footer-->
+  </body>
+</html>
---
+++ views/registrarMateria.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML>
+
+<html>
+	<link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
+	<link rel="stylesheet" type="text/css" href="Styles/Estilos.css">
+	<script src="semantic/dist/semantic.min.js"></script>
+
+	<head>
+		<title>Materias</title>
+	</head>
+
+	<body>
+    <div class="ui container">
+    </br>
+      <div class="ui sticky container segment fluid" style="padding-left:0; padding-right:0; padding-bottom:0;">
+        <div class="ui two column grid">
+          <div class="row">
+
+          <div class="one wide column">
+          </div>
+
+           <div class="three wide column">
+             <div class="ui small image">
+                <img src="imgs/logoucb.gif">
+              </div>
+            </div>
+
+            <div class="eleven wide column">
+              <div class="ui massive image">
+                <img src="imgs/Ingenieria-de-Sistemas.jpg">
+              </div>
+            </div>
+
+            <div class="one wide column">
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="ui inverted menu" style="margin:auto; background-color: #0B0B3B;">
+        <a class="item">Noticias</a>
+        <a class="item">Información General</a>
+        <a class="active item">Plantel Docente</a>
+        <a class="item">Cursos de Formación Continua</a>
+      </div>
+
+      <!--Form Materia-->
+      <div class="ui segment">
+        <center><h2>Registrar Materia</h2></center>
+      </br>
+        <form class="ui form">
+          <div class="field">
+              <div class="two fields">
+                <div class="field">
+                  <label>Código</label>
+                  <input type="number" name="codigo" placeholder="Ejemplo: 101">
+                </div>
+                <div class="field">
+                  <label>Sigla</label>
+                  <input type="text" name="sigla" placeholder="Ejemplo: INF-101">
+                </div>
+              </div>
+            </div>
+          <div class="field">
+            <label>Nombre</label>
+            <input type="text" name="nombre" placeholder="Nombre de Asignatura">
+          </div>
+          <center>
+            <button type="submit" class="positive ui button">Aceptar</button>
+            <button class="negative ui button">Cancelar</button>
+          </center>
+        </form>
+      </div>
+      <!--Fin Form-->
+
+    </div>
+  </br>
+  </br>
+    <!--Footer-->
+    <div class="ui center aligned inverted darkgreen vertical footer segment" style="background-color: #0B0B3B">
+       <div class="ui horizontal inverted small divided link list">
+           
+        </div>
+    </div>
+    <!--Fin Footer-->
+  </body>
+</html>
