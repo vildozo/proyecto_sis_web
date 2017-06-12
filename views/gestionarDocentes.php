f8084af9e76d03244cf41697d90462c857c48a76
Lesly Méndez C <leslicita_mc@hotmail.com>
2017-06-11 23:54:48 -0400

    Boton para nuevo docente en lista de docentes

--- views/gestionarDocentes.php
+++ views/gestionarDocentes.php
@@ -9,80 +9,80 @@
 	</head>
 
 	<body>
-
-	<div class="ui container">
-	</br>
-		<div class="ui sticky container segment fluid" style="padding-left:0; padding-right:0; padding-bottom:0;">
-			<div class="ui two column grid">
-				<div class="row">
-
-				<div class="one wide column">
-				</div>
-
-				 <div class="three wide column">
-					 <div class="ui small image">
-	  					<img src="imgs/logoucb.gif">
+		<div class="ui container">
+		</br>
+			<div class="ui sticky container segment fluid" style="padding-left:0; padding-right:0; padding-bottom:0;">
+				<div class="ui two column grid">
+					<div class="row">
+						<div class="one wide column">
 						</div>
-					</div>
 
-					<div class="eleven wide column">
-						<div class="ui massive image">
-							<img src="imgs/Ingenieria-de-Sistemas.jpg">
+						<div class="three wide column">
+							<div class="ui small image">
+			  				<img src="imgs/logoucb.gif">
+							</div>
 						</div>
-					</div>
 
-					<div class="one wide column">
-					</div>
+						<div class="eleven wide column">
+							<div class="ui massive image">
+								<img src="imgs/Ingenieria-de-Sistemas.jpg">
+							</div>
+						</div>
 
+						<div class="one wide column">
+						</div>
+
+					</div>
 				</div>
 			</div>
+
+			<div class="ui inverted menu" style="margin:auto; background-color: #0B0B3B;">
+				<a class="item">Noticias</a>
+				<a class="item">Información General</a>
+				<a class="active item">Plantel Docente</a>
+				<a class="item">Cursos de Formación Continua</a>
+			</div>
+
+			<!--Docente-->
+			<div class="ui segment">
+	      <center><h2>Lista de Docentes</h2></center>
+				<form action="registrarDocente.php">
+					<button class="ui right floated positive button">Nuevo Docente</button>
+				</form><br><br>
+				<?php
+					include("../controllers/listarDocentes.php")
+				 ?>
 		</div>
 
-		<div class="ui inverted menu" style="margin:auto; background-color: #0B0B3B;">
-			<a class="item">Noticias</a>
-			<a class="item">Información General</a>
-			<a class="active item">Plantel Docente</a>
-			<a class="item">Cursos de Formación Continua</a>
-		</div>
+	</br>
+		<!--Footer-->
+		<div class="ui sticky segment fluid inverted darkgreen vertical footer" style="background-color: #0B0B3B" >
+			 <div class="ui aligned container">
+				 <div class="ui stackable inverted divided grid">
 
-		<!--Docente-->
-		<div class="ui segment">
-      <h3>Lista de Docentes</h3>
-			<?php
-				include("../controllers/listarDocentes.php")
-			 ?>
-	</div>
+					 <div class="eight wide column">
+					 </div>
 
-</br>
-	<!--Footer-->
-	<div class="ui sticky segment fluid inverted darkgreen vertical footer" style="background-color: #0B0B3B" >
-		 <div class="ui aligned container">
-			 <div class="ui stackable inverted divided grid">
-
-				 <div class="eight wide column">
-				 </div>
-
-				 <div class="eight wide column">
-					 <div class="ui segment basic">
+					 <div class="eight wide column">
+						 <div class="ui segment basic">
 
 
-						 <h4 class="ui inverted header">Síguenos en:</h4>
-						 <div class="ui center aligned segment basic">
-							 <a class="ui circular facebook icon button"><i class="facebook icon"></i></a>
-							 <a class="ui circular twitter icon button"><i class="twitter icon"></i></a>
-							 <button class="ui circular google plus icon button"><i class="google plus icon"></i></button>
-							 <button class="ui circular instagram icon button"><i class="instagram icon"></i></button>
-							 <button class="ui circular youtube icon button"><i class="youtube icon"></i></button></br>
+							 <h4 class="ui inverted header">Síguenos en:</h4>
+							 <div class="ui center aligned segment basic">
+								 <a class="ui circular facebook icon button"><i class="facebook icon"></i></a>
+								 <a class="ui circular twitter icon button"><i class="twitter icon"></i></a>
+								 <button class="ui circular google plus icon button"><i class="google plus icon"></i></button>
+								 <button class="ui circular instagram icon button"><i class="instagram icon"></i></button>
+								 <button class="ui circular youtube icon button"><i class="youtube icon"></i></button></br>
+							 </div>
 						 </div>
 					 </div>
 				 </div>
+			 	</br></br></br>
+				<div class="ui inverted section divider"></div>
+			 </br></br></br></br>
 			 </div>
-		 </br></br></br>
-			 <div class="ui inverted section divider"></div>
-		 </br></br></br></br>
-		 </div>
-	</div>
- <!--Fin de Footer-->
-
+		</div>
+ 		<!--Fin de Footer-->
   </body>
 </html>
